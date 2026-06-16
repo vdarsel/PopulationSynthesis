a4c5d4c6c413b604c91dba09599f446f3fd9a851
vdarsel <vdarsel@live.fr>
2026-06-16 09:06:30 +0200

    Direct Inflating Verified

--- models/Direct_Inflating/Direct_Inflating.py
+++ models/Direct_Inflating/Direct_Inflating.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import os
 
 from utils.utils_sample import preprocessing_cat_data_dataframe_sampling
 
+from utils.utils_dir import get_file_path_sampling, get_data_dir
+from utils.utils_import import import_data, get_info_file
 
 def train_sample_Direct_Inflating(args):
     '''
@@ -17,36 +18,25 @@ def train_sample_Direct_Inflating(args):
     ##################
 
     term = "_Direct_Inflating"
-    datapath = "Data"
-    dataname = args.dataname
-    filename_training = args.filename_training
-    infoname = args.infoname
 
-    attr_setname = args.attributes_setname
+    filename_training = args.filename_training
+
     n_sample = args.n_generation
 
-    info_path = f'{datapath}/{dataname}/{infoname}'
-    
-    dataset_path = f'{datapath}/{dataname}/{filename_training}'
+    data_dir = get_data_dir(args)
         
-    filename_sampling = f"generated_population_{n_sample}.csv"
-
-    folder_sampling = f'{args.sample_folder}/{args.folder_save+term}'
-    sampling_file = f'{folder_sampling}/{filename_sampling}'
+    sampling_file = get_file_path_sampling(args, term)
     
-    if (not os.path.exists(folder_sampling)):
-        os.makedirs(folder_sampling)
-
     #################
     ### Load Data ###
     #################
 
-    info = pd.read_csv(info_path, sep = ";")
-    info = info[info[attr_setname]][["Type", "Variable_name", "Bin_size"]]
+    info = get_info_file(args)[["Type", "Variable_name", "Bin_size"]]
     
     name_cat = info[info["Type"].isin(["binary","category","bool"])].reset_index()["Variable_name"]
     
-    df_data = pd.read_csv(dataset_path, sep=";", low_memory=False)[info["Variable_name"]].astype(str)
+    columns = info["Variable_name"]
+    df_data = import_data(f"{data_dir}\\{filename_training}", columns, columns) # All variables are treated as categorical
     
     df_data, _ = preprocessing_cat_data_dataframe_sampling(df_data, args.transform.cat_min_count, name_cat)
 
